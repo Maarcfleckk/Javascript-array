172b1ad9a54e1039554849b151bea26b068ce052
Fleki <marc.fleck@mataro.epiaedu.cat>
2024-01-13 16:33:18 +0100

    Finish exercise

--- 00-imperative/js/getLongWords.js
+++ 00-imperative/js/getLongWords.js
@@ -8,3 +8,16 @@
 
 const LANGUAGES = ["Java", "C++", "JavaScript", "C#", "TypeScript"];
 const BEATLES = ["John", "George", "Paul", "Ringo"];
+
+export const getLongWords = function(LANGUAGES){
+    const longLanguages = [];
+    const collectionLenght = LANGUAGES.length;
+
+    for(let index = 0; index < collectionLenght; index++){
+        const Language = `${LANGUAGES[index]}`;
+        if(Language.length > 5){
+            longLanguages.push(Language);
+        }
+    }
+    return longLanguages;
+}
--- 00-imperative/js/getLongWords.test.js
+++ 00-imperative/js/getLongWords.test.js
@@ -1,3 +1,14 @@
-describe("", () => {
-  test("", () => {});
+import { getLongWords } from "./getLongWords.js";
+
+describe("Given getLongWords", () => {
+  test('When LANGUAGES array is provided as argument. Then expected array should be returned', () => {
+    //Arrange
+      const Languages = ["Java", "C++", "JavaScript", "C#", "TypeScript"]; 
+      const expectedResult = ['JavaScript', 'TypeScript']
+    //Act
+      const longLanguages = getLongWords(Languages);
+    //Assert
+      expect(longLanguages).toBeDefined();
+      expect(longLanguages).toEqual(expectedResult);
+  });
 });
