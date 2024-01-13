dd16a6614560d042a950de0b4eb72ad6361fb4b6
Fleki <marc.fleck@mataro.epiaedu.cat>
2024-01-13 17:13:58 +0100

    finish exercise

--- 01-map/capitalizeMates.test.js
+++ 01-map/capitalizeMates.test.js
@@ -1,3 +1,13 @@
-describe("", () => {
-  test("", () => {});
+import { capitalizeMates } from "./capitalizeMates.js";
+
+describe("Given capitalizeMates", () => {
+  test("When MATES array is provided as an argument. Then expected array should be returned", () => {
+    const MATES = ["john", "JACOB", "jinGleHeimer", "schmidt"];
+    const expectedResult = ["John", "Jacob", "Jingleheimer", "Schmidt"];
+
+    const mates = capitalizeMates(MATES);
+
+    expect(mates).toBeDefined();
+    expect(mates).toEqual(expectedResult);
+  });
 });
